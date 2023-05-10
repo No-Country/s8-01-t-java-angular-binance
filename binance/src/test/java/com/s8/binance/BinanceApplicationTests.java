1aa9b48122c26c2ec89827f940ad145f373b3451
marcos <marcosep192000@hotmail.com>
2023-05-10 18:28:32 -0300

    :art: create project

---
+++ binance/src/test/java/com/s8/binance/BinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s8.binance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
