03f4c31396c65957cff1df5a9c9ac9dbcbcf35be
leonelpb <leonelperez.b23@gmail.com>
2023-05-10 16:48:21 -0300

    add basic structure

--- binance-clone/src/app/app.module.ts
+++ binance-clone/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    DashboardComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
