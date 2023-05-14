82cc8cb5552deb97923d321de59da0b6d8370627
Federico <federicoburgos.dev@gmail.com>
2023-05-13 22:09:11 -0300

    working landing page

--- binance-clone/src/app/app-routing.module.ts
+++ binance-clone/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    component: LandingComponent,
+    title: 'Binance: Exchange de criptomonedas de Bitcoin, Ethereum y altcoins'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
