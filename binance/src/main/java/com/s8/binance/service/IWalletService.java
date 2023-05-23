749356f2c81749c7c40c02890931b79a9dd64bb7
marcos <marcosep192000@hotmail.com>
2023-05-23 18:54:49 -0300

    :bug: fix:  errors

--- binance/src/main/java/com/s8/binance/service/IWalletService.java
+++ binance/src/main/java/com/s8/binance/service/IWalletService.java
@@ -1,22 +1,11 @@
 package com.s8.binance.service;
 
-import java.util.List;
-
-import com.s8.binance.model.request.WalletRequestDto;
 import com.s8.binance.model.response.WalletResponseDto;
 import com.s8.binance.security.entity.Usuario;
 
 public interface IWalletService {
 
-    public List<WalletResponseDto> getAll();
-
-    public WalletResponseDto getWalletById(Long id);
-
-    public List<WalletResponseDto> getWalletsByFilters();
-
-    public WalletResponseDto createWallet(Usuario usuario);
-
-    // public WalletResponseDto updateWallet(Long id, WalletRequestDto WalletRequestDto);
+    public void  createWallet(Usuario usuario);
 
     public WalletResponseDto deleteWallet(Long id);
 }
--- binance/src/main/java/com/s8/binance/service/impl/WalletService.java
+++ binance/src/main/java/com/s8/binance/service/impl/WalletService.java
@@ -1,8 +1,5 @@
 package com.s8.binance.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import com.s8.binance.security.entity.Usuario;
@@ -10,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.s8.binance.model.entity.Wallet;
 import com.s8.binance.model.mapper.WalletMapper;
-import com.s8.binance.model.request.WalletRequestDto;
 import com.s8.binance.model.response.WalletResponseDto;
 import com.s8.binance.repository.IWalletRepository;
 import com.s8.binance.service.IWalletService;
@@ -20,54 +16,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 @Transactional
+
 public class WalletService implements IWalletService {
-
     private final IWalletRepository repository;
-
     private final WalletMapper mapper;
-
     @Override
-    public List<WalletResponseDto> getAll() {
-        List<Wallet> wallets = repository.findAll();
-        List<WalletResponseDto> WalletResponseDtoList = new ArrayList<>();
-        wallets.forEach(wallet -> {
-            WalletResponseDto response = mapper.fromEntityToDto(wallet);
-            WalletResponseDtoList.add(response);
-        });
-        return WalletResponseDtoList;
-    }
-
-    @Override
-    public WalletResponseDto getWalletById(Long id) {
-        Wallet wallet = repository.findById(id).orElseThrow();
-        WalletResponseDto response = mapper.fromEntityToDto(wallet);
-        return response;
-    }
-
-    @Override
-    public List<WalletResponseDto> getWalletsByFilters() {
-        throw new UnsupportedOperationException("Unimplemented method 'getWalletsByFilters'");
-    }
-
-    @Override
-    public WalletResponseDto createWallet(Usuario usuario) {
+    public void createWallet(Usuario usuario) {
         Wallet wallet = new Wallet();
         wallet.setUsuario(usuario);
         repository.save(wallet);
-        WalletResponseDto response = mapper.fromEntityToDto(wallet);
-        return response;
+      //  WalletResponseDto response = mapper.fromEntityToDto(wallet);
+
     }
-
-    // @Override
-    // @Transactional
-    // public WalletResponseDto updateWallet(Long id, WalletRequestDto walletRequestDto) {
-    //     Wallet wallet = repository.findById(id).orElseThrow();
-    //     Wallet updatedWallet = mapper.updateWallet(wallet, walletRequestDto);
-    //     repository.save(updatedWallet);
-    //     WalletResponseDto response = mapper.fromEntityToDto(updatedWallet);
-    //     return response;
-    // }
-
     @Override
     public WalletResponseDto deleteWallet(Long id) {
         repository.deleteById(id);
