945f59cc0c9cf055c3ba0dbfb369fce2bce84393
pulidodev <pulidodev@gmail.com>
2023-05-23 19:18:59 -0300

    backend fixes

--- binance/src/main/java/com/s8/binance/model/entity/Wallet.java
+++ binance/src/main/java/com/s8/binance/model/entity/Wallet.java
@@ -2,11 +2,21 @@ package com.s8.binance.model.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.s8.binance.security.entity.Usuario;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,13 +35,11 @@ public class Wallet {
     @Column(name = "ID_WALLET")
     private Long id;
     @OneToOne
-    @JoinColumn(name= "id_socio")
+    @JoinColumn(name = "id_socio")
     private Usuario usuario;
 
-@OneToMany(fetch = FetchType.EAGER, mappedBy = "wallets")
-@JsonIgnore
-List<Transaction> transactions =new ArrayList<>();
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "wallets")
+    @JsonIgnore
+    private List<Transaction> transactions = new ArrayList<>();
 
-    // relacion con usuario
-    // private User user;
 }
