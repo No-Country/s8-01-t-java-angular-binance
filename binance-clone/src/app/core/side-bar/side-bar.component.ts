42eb8ea242ffbfec3ead0a029f5462564ab92202
leonelpb <leonelperez.b23@gmail.com>
2023-05-25 18:48:09 -0300

    S0TJAB-43  add more fixes to sidebar

--- binance-clone/src/app/core/side-bar/side-bar.component.ts
+++ binance-clone/src/app/core/side-bar/side-bar.component.ts
@@ -15,7 +15,6 @@ import { faClose } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 interface SideNavToggle {
-  screenWidth: number;
   collapsed: boolean;
 }
 
@@ -24,33 +23,21 @@ interface SideNavToggle {
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent  {
 
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
-  screenWidth = 0;
 
-  @HostListener('window:resize', ['$event'])
-     onResize(event: any) {
-      this.screenWidth = window.innerWidth;
-        if(this.screenWidth <= 768 ) {
-          this.collapsed = false;
-          this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-        }
-      }
 
-  ngOnInit(): void {
-    this.screenWidth = window.innerWidth;
-  }
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    this.onToggleSideNav.emit({collapsed: this.collapsed});
   }
 
   closeSidenav(): void {
     this.collapsed = false;
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    this.onToggleSideNav.emit({collapsed: this.collapsed});
   }
 
   faUser= faUser;
