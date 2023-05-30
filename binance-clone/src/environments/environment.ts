02ff8341c35365c1bc0fb018ef93461af85e8877
braviabx3 <jcda23@gmail.com>
2023-05-29 23:23:04 -0500

    restructuracion de carpetas, se añadio archivos prettier, eslint, se crearon interceptors

--- binance-clone/src/environments/environment.ts
+++ binance-clone/src/environments/environment.ts
@@ -1,13 +1,4 @@
 export const environment = {
-  firebase: {
-    projectId: 'fir-img-fe824',
-    appId: '1:716334783373:web:4304eff21f29bf2d4cfbc4',
-    databaseURL: 'https://fir-img-fe824-default-rtdb.firebaseio.com',
-    storageBucket: 'fir-img-fe824.appspot.com',
-    apiKey: 'AIzaSyAsiDLPJzhCDz1AjqMVQqF-NEwyVp_oIkA',
-    authDomain: 'fir-img-fe824.firebaseapp.com',
-    messagingSenderId: '716334783373',
-  },
   apiUrl: '',
   baseUrl: 'https://api-nest-backend.up.railway.app',
   // baseUrl: 'http://localhost:5000/api',
