7191b972abde2ed29964ea2f8b08cedf3f99aeb9
Elcira Ibarra <elcira.rpi@gmail.com>
2023-06-01 00:37:54 -0300

    changes

--- binance-clone/src/app/services/auth.service.ts
+++ binance-clone/src/app/services/auth.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Register } from 'src/interfaces/register.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
+  apiUrl = environment.API_URL;
 
   constructor(
     private http: HttpClient
   ) { }
 
-  // signUpEmail(email: string) {
+  // signIn(email: string) {
   //   this.http.post<Auth>(`${this.apiUrl}/register`, { email });
   // }
 
-  // signUpPassword(password: string) {
-  //   this.http.post<Auth>(`${this.apiUrl}/register`, { password });
-  // }
+  signUp(register: Register) {
+    this.http.post<Register>(`${this.apiUrl}/register`, { register });
+  }
 
 
 }
