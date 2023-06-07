bf97fbeb6f6d6f00c1593f70b7ee4a44efbd92c5
Fernando Weisheim <fmatias591@gmail.com>
2023-06-06 23:21:12 -0300

    terminada para presentar

--- binance-clone/src/app/pages/crypto/components/sell/sell.component.ts
+++ binance-clone/src/app/pages/crypto/components/sell/sell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faCcMastercard } from '@fortawesome/free-brands-svg-icons';
 import { faArrowRight, faHourglassHalf } from '@fortawesome/free-solid-svg-icons';
 import { forEach } from 'lodash';
@@ -10,7 +11,10 @@ import { CryptoService } from 'src/app/services/crypto.service';
   styleUrls: ['./sell.component.scss']
 })
 export class SellComponent implements OnInit {
-
+  mostrarDiv: boolean = true;
+  divProcesando:boolean=true;
+  divVenta:boolean=true;
+  divVentaExitosa=true;
   ARS = [];
   BTC=[];
   total=0;
@@ -21,19 +25,21 @@ export class SellComponent implements OnInit {
   resultadoMultiplicacion=0;
   cantidadCripto=0;
 
-  constructor(private getCoin: CryptoService) {
+  inputValor=0;
+
+  constructor(private getCoin: CryptoService,private router:Router) {
 
   }
   ngOnInit() {
     this.getARS();
     // this.getBTC();
-   
+  
   }
 
    mostrarValorConsola() {
     // console.log(this.cantidadCripto);
-    let inputValor = this.cantidadCripto;
-    let totalCompra = inputValor * this.valorCripto;
+    this.inputValor = this.cantidadCripto;
+    let totalCompra = this.inputValor * this.valorCripto;
     this.resultadoMultiplicacion=totalCompra;
     console.log(totalCompra);
    }
@@ -52,28 +58,51 @@ export class SellComponent implements OnInit {
       
       }
     });
-  // getARS() {
-  //   this.getCoin.getCoins().subscribe({
-  //     next: (res: any) => {
-  //       this.ARS = res;
-  //       console.log(this.ARS = res.filter((crypto: any) => crypto.name === 'ARS' || crypto.name === 'BTC'));
-  //     }
-  //   });
-  // }
 
-  // getBTC() {
-  //   this.getCoin.getCoins().subscribe({
-  //     next: (res: any) => {
-  //       this.BTC = res;
-  //       console.log(this.BTC);
-  //     }
-  //   });
-  // }
 
 
 
 
 
 }
+
+
+      ocultar(){
+        this.mostrarDiv=!this.mostrarDiv;
+      }
+
+      volverDiv(){
+        this.mostrarDiv=this.mostrarDiv=true;
+      }
+
+      procesando(){
+        this.divVenta=!this.divVenta;
+        this.divProcesando =!this.divProcesando;
+        this.VentaRealizada();
+      }
+    
+      VentaRealizada(){
+        setTimeout(()=>{
+          this.divProcesando=true
+          this.divVentaExitosa=!this.divVentaExitosa
+          setTimeout(() => {
+            this.redirigir();
+          },3000 );
+        },3000)
+
+        // setTimeout(() => {
+        // this.divProcesando=true
+        // this.divVentaExitosa=!this.divVentaExitosa
+        // }, 3000);
+        // setTimeout(() => {
+        //  this.redirigir();
+        //   }, 3000);
+
+      }
+
+      redirigir(){
+       this.router.navigate(['/landing'])
+      }
+      
 }
 
