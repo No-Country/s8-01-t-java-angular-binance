ca4de51f3d103c77d1d9a745bd86b4056c4fa5d0
Fernando Weisheim <fmatias591@gmail.com>
2023-06-06 21:50:38 -0300

    calcular precio por bitcoint

--- binance-clone/src/app/pages/crypto/components/sell/sell.component.ts
+++ binance-clone/src/app/pages/crypto/components/sell/sell.component.ts
@@ -13,43 +13,67 @@ export class SellComponent implements OnInit {
 
   ARS = [];
   BTC=[];
+  total=0;
+  valorCripto=0;
   faMastercard = faCcMastercard;
   faRight = faArrowRight;
   faSand = faHourglassHalf;
-
-
+  resultadoMultiplicacion=0;
+  cantidadCripto=0;
 
   constructor(private getCoin: CryptoService) {
 
   }
   ngOnInit() {
     this.getARS();
-    this.getBTC();
+    // this.getBTC();
+   
   }
 
-
+   mostrarValorConsola() {
+    // console.log(this.cantidadCripto);
+    let inputValor = this.cantidadCripto;
+    let totalCompra = inputValor * this.valorCripto;
+    this.resultadoMultiplicacion=totalCompra;
+    console.log(totalCompra);
+   }
+  // }
   getARS() {
     this.getCoin.getCoins().subscribe({
       next: (res: any) => {
         this.ARS = res;
         console.log(this.ARS = res.filter((crypto: any) => crypto.name === 'ARS' || crypto.name === 'BTC'));
+        console.log(this.ARS[0]['usdValue']);
+        this.valorCripto= parseFloat(this.ARS[0]['usdValue']); 
+      
+  
+      
+        return(this.total);
+      
       }
     });
-  }
+  // getARS() {
+  //   this.getCoin.getCoins().subscribe({
+  //     next: (res: any) => {
+  //       this.ARS = res;
+  //       console.log(this.ARS = res.filter((crypto: any) => crypto.name === 'ARS' || crypto.name === 'BTC'));
+  //     }
+  //   });
+  // }
 
-  getBTC() {
-    this.getCoin.getCoins().subscribe({
-      next: (res: any) => {
-        this.BTC = res;
-        console.log(this.BTC);
-      }
-    });
-  }
+  // getBTC() {
+  //   this.getCoin.getCoins().subscribe({
+  //     next: (res: any) => {
+  //       this.BTC = res;
+  //       console.log(this.BTC);
+  //     }
+  //   });
+  // }
 
 
 
 
 
 }
-
+}
 
