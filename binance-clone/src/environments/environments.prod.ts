04bc68349b31a9e61c71ac4911351c6008f3d0e6
Juan Da Silva <jcda_23@hotmail.com>
2023-06-08 12:51:06 -0500

    Update environments.prod.ts

--- binance-clone/src/environments/environments.prod.ts
+++ binance-clone/src/environments/environments.prod.ts
@@ -1,11 +1,1 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://binance.up.railway.app/api/v1',
 
-  ipfs: 'https://ipfs.infura.io:5001',
-  ipfs2: 'https://nocountry.infura-ipfs.io',
-  ipfsKey: '2QFujw28oy1ninclr6ESxPeBcyD',
-  ipfsKeyS: '001f43d04b968cf53d2dfae000c7a44b',
-
-  contractAddress: '0x55FEdA15621E6aA3862E445fD96B757e392a7b71',
-};
